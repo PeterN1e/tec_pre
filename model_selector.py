e089cac173de7bfd3ad2556039d88349dd912dd7
PeterN1e <1454218249@qq.com>
2026-07-15 00:32:40 +0800

    小修改

--- model_selector.py
+++ model_selector.py
@@ -6,13 +6,13 @@ USE_MODEL = cfg_model.model_name
 
 # 统一导入对应模型
 if USE_MODEL == "transformer":
-    from Transformer.transformerModule import TecPreTransformer as Model_Predictor
+    from PredictionModel.Transformer.transformerModule import TecPreTransformer as Model_Predictor
 elif USE_MODEL == "tcn":
-    from TCN.tcnModule import TCNMiddlePredictor as Model_Predictor
+    from PredictionModel.TCN.tcnModule import TCNMiddlePredictor as Model_Predictor
 elif USE_MODEL == "convlstm":
-    from LSTM.convLSTM.convLSTM import ConvLSTM as Model_Predictor
+    from PredictionModel.LSTM.convLSTM.convLSTM import ConvLSTM as Model_Predictor
 elif USE_MODEL == "convgru":
-    from Gru.convGRU import ConvGRU as Model_Predictor
+    from PredictionModel.Gru.convGRU import ConvGRU as Model_Predictor
 else:
     raise ValueError("模型不存在")
 
